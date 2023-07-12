5ee29126d55637d2357b1e90f8922910078147fd
Ibrahim Fathi <himafathy796@gmail.com>
2023-07-13 02:45:45 +0300

    [Feature] Show posts in home screen create animation widget for likes

---
+++ lib/screens/feed_screen.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:instagram_clone/generated/assets.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/post_card.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              FontAwesomeIcons.facebookMessenger,
+            ),
+          ),
+        ],
+        title: SvgPicture.asset(
+          Assets.assetsIcInstagram,
+          color: primaryColor,
+          height: 30,
+        ),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("posts").snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            physics: BouncingScrollPhysics(),
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) => PostCard(
+              snap: snapshot.data!.docs[index].data(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
